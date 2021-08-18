ae59ab86de38aa9c9efd84e3ea3d692d78f821af
Karl Li <killtw@gmail.com>
2021-08-18 17:05:42 +0800

    add migration

---
+++ database/migrations/2021_08_18_080005_create_records_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->bigInteger('merchant_id');
+            $table->string('from');
+            $table->timestamp('time');
+            $table->string('geohash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
