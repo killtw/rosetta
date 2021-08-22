80f3d61b4dd4182b2519de062c28fbc2fc1c8cb2
Karl Li <killtw@gmail.com>
2021-08-22 22:26:38 +0800

    add test

--- tests/Feature/MerchantsTest.php
+++ tests/Feature/MerchantsTest.php
@@ -111,4 +111,32 @@ class MerchantsTest extends TestCase
             'address' => 'The address field is required when location is not present.',
         ]);
     }
+
+    /** @test */
+    public function it_should_covert_address_to_coordinate()
+    {
+        // arrange
+        $expected = [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->phoneNumber,
+            'identity' => 'A123456789',
+            'address' => '台北市信義區市府路1號',
+        ];
+
+        // act
+        $actual = $this->postJson(route('merchants.store'), $expected);
+
+        // assert
+        $actual
+            ->assertCreated()
+            ->assertJson([
+                'message' => 'success',
+                'data' => [
+                    'location' => [
+                        'lat' => 25.0381727,
+                        'lng' => 121.5643485,
+                    ],
+                ],
+            ]);
+    }
 }
