c89033eaa65116bab27982560ad915c08f2ea9d8
Karl Li <killtw@gmail.com>
2021-08-22 23:16:42 +0800

    add service test case

--- tests/Unit/Services/MerchantServiceTest.php
+++ tests/Unit/Services/MerchantServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit\Services;
 use App\Services\MerchantService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Geocoder\Facades\Geocoder;
 use Tests\TestCase;
 
 class MerchantServiceTest extends TestCase
@@ -30,6 +31,32 @@ class MerchantServiceTest extends TestCase
 
         // assert
         $this->assertSame($expected['name'], $actual->name);
+        $this->assertDatabaseHas('merchants', ['name' => $expected['name']]);
+    }
+
+    /** @test */
+    public function it_should_create_a_merchant_by_address()
+    {
+        // arrange
+        $expected = [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->phoneNumber,
+            'identity' => 'A123456789',
+            'address' => '台北市信義區市府路1號',
+        ];
+        Geocoder::shouldReceive('getCoordinatesForAddress')
+            ->with($expected['address'])
+            ->andReturn([
+                'lat' => 25.0381727,
+                'lng' => 121.5643485,
+            ]);
+
+        // act
+        $actual = app(MerchantService::class)->create($expected);
+
+        // assert
+        $this->assertSame(25.0381727, $actual->location['lat']);
+        $this->assertSame(121.5643485, $actual->location['lng']);
         $this->assertDatabaseHas('merchants', ['name' => $expected['name']]);
     }
 }
