d80285e6c23dca7cae8db532082edf475a81455f
Karl Li <killtw@gmail.com>
2021-08-22 23:18:22 +0800

    feat(merchant): implement address to coordinates (#9)
    * add test
    
    * add spatie/geocoder
    
    * mock geocoder facade
    
    * pass address to service
    
    * implement getCoordinates method
    
    * add service test case
    
    * fix location default value

--- tests/Feature/MerchantsTest.php
+++ tests/Feature/MerchantsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Spatie\Geocoder\Facades\Geocoder;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -110,5 +111,39 @@ class MerchantsTest extends TestCase
             'location' => 'The location field is required when address is not present.',
             'address' => 'The address field is required when location is not present.',
         ]);
+    }
+
+    /** @test */
+    public function it_should_covert_address_to_coordinate()
+    {
+        // arrange
+        $expected = [
+            'name' => $this->faker->name,
+            'phone' => $this->faker->phoneNumber,
+            'identity' => 'A123456789',
+            'address' => '台北市信義區市府路1號',
+        ];
+        Geocoder::shouldReceive('getCoordinatesForAddress')
+            ->with($expected['address'])
+            ->andReturn([
+                'lat' => 25.0381727,
+                'lng' => 121.5643485,
+            ]);
+
+        // act
+        $actual = $this->postJson(route('merchants.store'), $expected);
+
+        // assert
+        $actual
+            ->assertCreated()
+            ->assertJson([
+                'message' => 'success',
+                'data' => [
+                    'location' => [
+                        'lat' => 25.0381727,
+                        'lng' => 121.5643485,
+                    ],
+                ],
+            ]);
     }
 }
