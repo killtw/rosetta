eb8365df31016b80d09d388e7247bb905ca950e2
Karl Li <killtw@gmail.com>
2021-08-29 16:33:05 +0800

    implement records.store

--- app/Http/Controllers/RecordController.php
+++ app/Http/Controllers/RecordController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateRecordRequest;
 use App\Models\Record;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Str;
+use Symfony\Component\HttpFoundation\Response;
 
 class RecordController extends Controller
 {
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \App\Http\Requests\CreateRecordRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function store(CreateRecordRequest $request): JsonResponse
     {
-        //
+        Record::create([
+            'uuid' => Str::uuid(),
+            'merchant_id' => data_get($request->all(), 'merchant_id'),
+            'from' => data_get($request->all(), 'from'),
+            'time' => $request->get('time'),
+        ]);
+
+        return response()->json(['message' => 'success'])->setStatusCode(Response::HTTP_CREATED);
     }
 }
