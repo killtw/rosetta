53fa82773586ab184958ea6a195e1c191e84d212
Karl Li <killtw@gmail.com>
2021-08-30 15:11:39 +0800

    add addRecordToMerchant test case

--- tests/Unit/Services/RedisServiceTest.php
+++ tests/Unit/Services/RedisServiceTest.php
@@ -1,13 +1,16 @@
 <?php
 
-namespace Tests\Unit\Merchant;
+namespace Tests\Unit\Services;
 
 use App\Services\RedisService;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Redis;
 use Tests\TestCase;
 
 class RedisServiceTest extends TestCase
 {
+    use WithFaker;
+
     /** @test */
     public function it_should_add_coordinate_to_redis()
     {
@@ -46,5 +49,23 @@ class RedisServiceTest extends TestCase
         // assert
         $this->assertEquals($expected[0][0], $actual['lng']);
         $this->assertEquals($expected[0][1], $actual['lat']);
+    }
+
+    /** @test */
+    public function it_should_add_uuid_to_sorted_set()
+    {
+        // arrange
+        $expected = [
+            'merchant_id' => 1,
+            'from' => $this->faker->uuid,
+            'time' => now()->timestamp,
+        ];
+
+        // act
+        app(RedisService::class)->addRecordToMerchant($expected['merchant_id'], $expected['from'], $expected['time']);
+        $actual = Redis::command('zscore', [sprintf(RedisService::MERCHANTS_KEY . ':%s', $expected['merchant_id']), $expected['from']]);
+
+        // assert
+        $this->assertEquals($expected['time'], $actual);
     }
 }
