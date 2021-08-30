8837bab891d5eff38854bd8245f1d9ac4ff4e4ac
Karl Li <killtw@gmail.com>
2021-08-30 12:04:58 +0800

    implement RecordService

---
+++ domain/Records/RecordService.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Domain\Records;
+
+use App\Models\Record;
+use Str;
+
+class RecordService
+{
+    public function create(array $data)
+    {
+        Record::create([
+            'uuid' => Str::uuid(),
+            'merchant_id' => data_get($data, 'merchant_id'),
+            'from' => data_get($data, 'from'),
+            'time' => data_get($data, 'time'),
+        ]);
+    }
+}
